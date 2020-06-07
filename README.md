fabada272ec8960dc5af801be13fc71c4c0ff31b
James Rezendes <jamesrezendes@Jamess-MacBook-Pro.local>
2020-06-06 19:59:10 -0700

    ready for heroku deploy

---
+++ README.md
@@ -0,0 +1,2 @@
+# Valoria Peer Server
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
--- server.js
+++ server.js
@@ -1,45 +1,15 @@
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
 require('dotenv').config();
-var data = null;
 
 const app = express();
 
-app.get('/', (req, res, next) => res.send('Hello world!'));
+app.get('/', (req, res, next) => res.send("Hi! I'm the Valorian Peer Server"));
 
 const server = app.listen(process.env.PORT);
 
-const startPeerServer = () => {
-  const peerServer = ExpressPeerServer(server, {
-    debug: true,
-    path: '/valoria'
-  });
-  app.use('/peerjs', peerServer);
-}
-
-const AWS = require('aws-sdk');
-var s3 = null;
-
-if(!process.env.DEVELOPMENT){
-  AWS.config.update({region: 'us-west-1'});
-  s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-  });
-  s3.getObject({Bucket : "valoria", Key : "data.json"}, function(err, fileData) {
-    if(err) console.log(err, err.stack);
-    else {
-      data = JSON.parse(fileData.Body.toString())
-      startPeerServer();
-    }
-  })
-}
-
-function saveData(cb) {
-  if(!process.env.DEVELOPMENT){
-    s3.upload({Bucket : "valoria", Key : "data.json", Body : JSON.stringify(data, null, 2)}, (err, fileData) => {
-      if (err) console.error(`Upload Error ${err}`);
-      if(cb && typeof cb == 'function') cb();
-    });
-  }
-}
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/valoria'
+});
+app.use('/peerjs', peerServer);
